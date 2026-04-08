e79f86970490cd6d54de0055ee035bd087e860cb
Seth <sethurajcs14@gmail.com>
2026-04-09 02:03:14 +0530

    Organizing project structure and adding Day 2

---
+++ day2/src/devops_tools.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# The line above is the "Shebang". It tells the Mac: "Use Bash to run this."
+
+echo "Hello! I am running from the src folder."
+echo "Current Date and Time is:"
+date
---
+++ day2/test_prep/timer.sh
@@ -0,0 +1,7 @@
+
+
+echo "Starting timer..."
+(sleep 5)
+echo "Time is up!"
+date
+
